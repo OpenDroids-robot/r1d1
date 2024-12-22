5d977d50d4520d589529beec27e913ca7aad5f12
Shubh152 <shubhj013@gmail.com>
2024-12-22 11:39:39 +0530

    Added transform publisher

--- aruco_marker/README.md
+++ aruco_marker/README.md
@@ -3,6 +3,10 @@ Detects aruco markers from ros2 image messages and publishes
 - /detected_markers/marker_pose --> for detected pose and id as PoseStamped msgs
 - /detected_markers/image --> draws detected markers and axis as Image msgs
 
+##### Commit : Added transform publisher
+
+Also publishes **TFMessages** to **/tf** from **_marker\_<id>\_frame_** to **_<camera_frame>_** (modified in detect_markers.py by the user)
+
 **Works for video feed from camera**
 (modify camera intrinsic parameters and distortion coefficients hardcoded inside detect_markers.py and change image topic in subscriber)
 
--- aruco_marker/aruco_marker/detect_markers.py
+++ aruco_marker/aruco_marker/detect_markers.py
@@ -4,7 +4,8 @@ from rclpy.node import Node
 from cv_bridge import CvBridge, CvBridgeError
 import rclpy.time
 from sensor_msgs.msg import Image
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import TransformStamped
+from tf2_msgs.msg import TFMessage
 import tf_transformations
 
 class ArucoDetector(Node):
@@ -26,9 +27,15 @@ class ArucoDetector(Node):
             10
         )
 
-        self.pose_publisher = self.create_publisher(
-            PoseStamped,
-            "/detected_marker/marker_pose",
+        self.transform_publisher = self.create_publisher(
+            TransformStamped,
+            "/detected_marker/transforms",
+            10
+        )
+
+        self.tf_publisher = self.create_publisher(
+            TFMessage,
+            "/tf",
             10
         )
 
@@ -42,7 +49,7 @@ class ArucoDetector(Node):
         self.camera_matrix = msg.k
         self.dist_coeffs = msg.d
         self.get_logger().info(f"k : {msg.k}")
-        self.get_logger().info(f"k : {msg.d}")
+        self.get_logger().info(f"d : {msg.d}")
 
     def rgb_callback(self,msg):
         # Camera intrinsic parameters obtained fron camera_info topic in gazebo
@@ -53,7 +60,7 @@ class ArucoDetector(Node):
         aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_1000)
         parameters = cv2.aruco.DetectorParameters()
         try:
-            # Convert the ROS depth image message to an OpenCV image
+            # Convert the ROS rgb image message to an OpenCV image
             rgb_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
             gray = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2GRAY)
             detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
@@ -70,30 +77,33 @@ class ArucoDetector(Node):
                         if distance < 0.0001 or distance > 1000:
                             self.video_publisher.publish(msg)
                         else:
-                        # self.get_logger().info(f"Detected marker: {id}\nRotation : {rvec}\nTranslation : {tvec}")
                             cv2.drawFrameAxes(rgb_image,self.camera_matrix,self.dist_coeffs,rvec,tvec,1,3)
                             cv2.aruco.drawDetectedMarkers(rgb_image,corners,ids)
-                            # To publish pose
+                            # To publish transform
                             rotation_matrix = np.array([[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 1]],dtype=float)
                             rotation_matrix[:3, :3], _ = cv2.Rodrigues(rvec)
-
+                            tf_message = TFMessage()
                             # convert the matrix to a quaternion
                             quaternion = tf_transformations.quaternion_from_matrix(rotation_matrix)
-                            pose = PoseStamped()
-                            pose.header.stamp = self.get_clock().now().to_msg()
-                            pose.header.frame_id = f"Detected ID : {id} Distance : {distance}"
-                            pose.pose.position.x = float(tvec[0])
-                            pose.pose.position.y = float(tvec[0])
-                            pose.pose.position.z = float(tvec[2])
-                            pose.pose.orientation.x = float(quaternion[0])
-                            pose.pose.orientation.y = float(quaternion[1])
-                            pose.pose.orientation.z = float(quaternion[2])
-                            pose.pose.orientation.w = float(quaternion[3])
-                            self.pose_publisher.publish(pose)
+                            transform = TransformStamped()
+                            transform.header.stamp = self.get_clock().now().to_msg()
+                            transform.child_frame_id = "<put_camera_frame_id>"
+                            transform.header.frame_id = f"marker_{id[0]}_frame"
+                            transform.transform.translation.x = float(tvec[0])
+                            transform.transform.translation.y = float(tvec[1])
+                            transform.transform.translation.z = float(tvec[2])
+                            transform.transform.rotation.x = float(quaternion[0])
+                            transform.transform.rotation.y = float(quaternion[1])
+                            transform.transform.rotation.z = float(quaternion[2])
+                            transform.transform.rotation.w = float(quaternion[3])
+                            tf_message.transforms.append(transform)
+                            self.transform_publisher.publish(transform)
+                            self.tf_publisher.publish(tf_message)
                     else:
-                        self.get_logger().error(f"Unable to estimate pose of markers")     
-                    ros_msg = self.bridge.cv2_to_imgmsg(rgb_image, encoding="bgr8")  # Convert the OpenCV image to a ROS Image message                       
-                    self.video_publisher.publish(ros_msg)
+                        self.get_logger().error(f"Unable to estimate pose of markers")  
+
+                ros_msg = self.bridge.cv2_to_imgmsg(rgb_image, encoding="bgr8")  # Convert the OpenCV image to a ROS Image message                          
+                self.video_publisher.publish(ros_msg)
             else:        
                 self.video_publisher.publish(msg)
         except CvBridgeError as e:
