87c8c459a9bfafcc76e78f7adadf1882e5aa0f92
cruxbots <mishrarahul1997@yahoo.com>
2024-12-28 01:25:16 +0530

    add controller

--- r1d1_description/launch/gazebo.launch.py
+++ r1d1_description/launch/gazebo.launch.py
@@ -11,7 +11,7 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     share_dir = get_package_share_directory('r1d1_description')
 
-    xacro_file = os.path.join(share_dir, 'urdf', 'r1d1.xacro')
+    xacro_file = os.path.join(share_dir, 'urdf', 'r1d1_m.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     robot_urdf = robot_description_config.toxml()
     urdf_path = os.path.join(share_dir, 'urdf', 'r1d1.urdf')
@@ -94,6 +94,13 @@ def generate_launch_description():
             "arm_controller", "-c", "/controller_manager"
         ]
     )
+    slider_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            "slider_controller", "-c", "/controller_manager"
+        ]
+    )
     gripper_controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
@@ -101,12 +108,26 @@ def generate_launch_description():
             "gripper_controller", "-c", "/controller_manager"
         ]
     )
-
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            "joint_state_broadcaster", "-c", "/controller_manager"
+        ],
+        parameters=[
+            {'use_sim_time': True}
+        ],
+    )
     return LaunchDescription([
         robot_state_publisher_node,
         joint_state_publisher_node,
         gazebo_server,
         gazebo_client,
         urdf_spawn_node,
-        ros_gz_bridge
+        ros_gz_bridge,
+        joint_state_broadcaster_spawner,
+        arm_controller_spawner,
+        slider_controller_spawner,
+        gripper_controller_spawner,
+        
     ])
