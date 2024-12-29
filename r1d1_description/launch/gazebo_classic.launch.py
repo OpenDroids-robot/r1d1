2fc149f3ddd551ad8ec149ac206e3f89f8d6cc3f
ishanph2005 <149067003+ishanph2005@users.noreply.github.com>
2024-12-29 21:34:49 +0530

    added ignition controller code

--- r1d1_description/launch/gazebo_classic.launch.py
+++ r1d1_description/launch/gazebo_classic.launch.py
@@ -12,18 +12,22 @@ def generate_launch_description():
     share_dir = get_package_share_directory('r1d1_description')
 
     xacro_file = os.path.join(share_dir, 'urdf', 'r1d1_m.xacro')
-    doc = xacro.parse(open(xacro_file))
-    xacro.process_doc(doc)
-    
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_urdf = robot_description_config.toxml()
+    use_sim_time = LaunchConfiguration('use_sim_time', default='True')
     urdf_path = os.path.join(share_dir, 'urdf', 'r1d1_m.urdf')
-    world = os.path.join(share_dir,'world','world_with_aruco.sdf')
+    world = os.path.join(share_dir,'world','world_with_aruco.world')
+    with open(urdf_path, 'w') as urdf_file:
+        urdf_file.write(robot_urdf)
+    print("URDF file generated successfully")
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
         parameters=[
-            {'robot_description': doc.toxml()}
+            {'robot_description': robot_urdf,
+             'use_sim_time': use_sim_time,}
         ]
     )
 
@@ -36,34 +40,33 @@ def generate_launch_description():
     gazebo_server = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             PathJoinSubstitution([
-                FindPackageShare('ros_gz_sim'),
+                FindPackageShare('gazebo_ros'),
                 'launch',
-                'gz_sim.launch.py'
+                'gzserver.launch.py'
             ])
         ]),
         launch_arguments={
             'pause': 'false',
-            'gz_args': ['-r -s -v 4', world],
+            'use_sim_time': 'true',
+            'world': world
         }.items()
     )
 
     gazebo_client = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             PathJoinSubstitution([
-                FindPackageShare('ros_gz_sim'),
+                FindPackageShare('gazebo_ros'),
                 'launch',
-                'gz_sim.launch.py'
+                'gzclient.launch.py'
             ])
-        ]),
-        launch_arguments={'gz_args': '-g '}.items()
+        ])
     )
 
     urdf_spawn_node = Node(
-        package='ros_gz_sim',
-        executable='create',
+        package='gazebo_ros',
+        executable='spawn_entity.py',
         arguments=[
-            '-name', 'r1d1',
-            '-file', urdf_path,
+            '-entity', 'r1d1',
             '-topic', 'robot_description'
         ],
         parameters=[
@@ -71,42 +74,7 @@ def generate_launch_description():
         ],
         output='screen'
     )
-    
-    ros_gz_bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=[
-            #'/lidar_data@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
-            #'/lidar_data/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked',
-            '/rgbd_camera/image@sensor_msgs/msg/Image@gz.msgs.Image',
-            '/rgbd_camera/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo',
-            '/rgbd_camera/depth_image@sensor_msgs/msg/Image@gz.msgs.Image',
-            '/rgbd_camera/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked'
-        ],
-        output='screen'
-    )
-    
-    arm_controller_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=[
-            "arm_controller", "-c", "/controller_manager"
-        ]
-    )
-    slider_controller_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=[
-            "slider_controller", "-c", "/controller_manager"
-        ]
-    )
-    gripper_controller_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=[
-            "gripper_controller", "-c", "/controller_manager"
-        ]
-    )
+
     joint_state_broadcaster_spawner = Node(
         package='controller_manager',
         executable='spawner',
@@ -117,6 +85,27 @@ def generate_launch_description():
             {'use_sim_time': True}
         ],
     )
+    slider_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            "slider_controller", "-c", "/controller_manager"
+        ]
+    )
+    arm_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            "arm_controller", "-c", "/controller_manager"
+        ]
+    )
+    gripper_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            "gripper_controller", "-c", "/controller_manager"
+        ]
+    )
 
     return LaunchDescription([
         robot_state_publisher_node,
@@ -124,10 +113,8 @@ def generate_launch_description():
         gazebo_server,
         gazebo_client,
         urdf_spawn_node,
-        ros_gz_bridge,
         joint_state_broadcaster_spawner,
-        arm_controller_spawner,
         slider_controller_spawner,
-        gripper_controller_spawner
+        gripper_controller_spawner,
+        arm_controller_spawner,
     ])
-
